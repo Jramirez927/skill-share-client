5e28270d87d1587ad78981562e1e04f3497dd50e
Jorge Ramirez <jorgeramirez927@gmail.com>
2024-06-21 16:21:22 -0400

    Layout for tabs route

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Tabs, Redirect } from
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>TabsLayout</Text>
+    </View>
+  )
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
